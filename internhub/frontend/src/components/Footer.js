5f86854a02c6d74dadd963d7a60202a299dee26c
Harsh Patel <hpatel25177@gmail.com>
2026-02-16 14:31:25 +0530

    New code added.

--- internhub/frontend/src/components/Footer.js
+++ internhub/frontend/src/components/Footer.js
@@ -42,7 +42,7 @@ const Footer = () => (
       </div>
 
       <div className="footer-bottom">
-        <p>© 2024 InternHub. All rights reserved.</p>
+        <p>© 2026 InternHub. All rights reserved.</p>
         <div className="footer-links">
           <a href="#">Privacy Policy</a>
           <a href="#">Terms of Service</a>
